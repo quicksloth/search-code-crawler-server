2e4e2b61b329a800b770eb8dcef0cf73e17d7691
ahmadk3 <ahmad.aktb@gmail.com>
2017-05-09 20:36:00 -0300

    <pre> extraction enabled

--- app/crawler.rb
+++ app/crawler.rb
@@ -1,20 +1,36 @@
 require 'google_custom_search_api'
 require 'openssl'
+require 'net/http'
+require 'open-uri'
+require 'nokogiri'
 
 
 module Crawler
 
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
+  #faz a query no google e retorna os links da pesquisa
   def self.search(query, opts = {})
 
     results = GoogleCustomSearchApi.search(query, opts)
     links = []
-
     results["items"].each do |item|
-      links.push({link:item["link"], title:item["title"]})
+      links.push({link:item["link"], title:item["title"], snippet:item["snippet"]})
     end
 
     return links
+  end
+
+  #extrai documentação e o código fonte da página a partir do html
+  def self.extractHTML(links)
+
+    htmls = []
+
+    #links.each do |link|
+      puts links[0][:link]
+      doc = Nokogiri::HTML(open(links[0][:link]))
+      htmls.push(doc)
+    #end
+    return htmls
   end
 end
--- app/main.rb
+++ app/main.rb
@@ -1,3 +1,23 @@
 require_relative 'crawler'
 
-puts Crawler.search("how to read a file")
+links = Crawler.search("how to read a file ruby")
+htmls = Crawler.extractHTML(links)
+
+h = htmls[0].to_s
+start = h.index('<pre>')
+finish = h.index('</pre>')
+v = []
+while start != nil && finish != nil
+  v.push h[start..finish + 5]
+
+  h = h[finish + 5..h.size]
+
+  start = h.index('<pre>')
+  finish = h.index('</pre>')
+
+end
+
+puts v[1]
+
+
+

5bee079f2be7538d50b3b63b83a6c6c4376a84b3
ahmadk3 <ahmad.aktb@gmail.com>
2017-09-08 21:15:16 -0300

    teste setup

--- controller/crawler_controller.rb
+++ controller/crawler_controller.rb
@@ -27,22 +27,46 @@ end
 
 @request_payload = ''
 
-before '/teste' do
+before '/crawl' do
   puts "Request received"
   request.body.rewind
   @request_payload = request.body.read
 end
 
-get '/teste' do
+get '/crawl' do
   "Crawler instantiated"
 end
 
-after '/teste' do
+after '/crawl' do
   json = JSON.parse(@request_payload)
-  data = everything(json["query"], json["id"])
+  #data = everything(json["query"], json["id"])
+
+  data = {
+      "requestID": request_body.get('requestID'),
+      'searchResult': [
+          {
+              'documentation': 'reading a file',
+              'url': 'https://url.com',
+              'sourceCode': [
+                  '''import json\n''',
+                  '''import json\n\n\n\n\n\n\n\n\n\n\n\n\n''',
+                  '''import json\nfrom uuid import uuid4\n# you may also want''',
+                  '''with open(fname) as f:\n    content = f.readlines()\n# you may also want to remove whitespace characters like `\\n` at the end of each line\ncontent = [x.strip() for x in content] \nprint(2)\n'''
+              ],
+          },
+          {
+              'documentation': 'When you’re working with Python, you don’t need to import a library in order to read and write files. It’s handled natively in the language, albeit in a unique manner.',
+              'url': 'http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python',
+              'sourceCode': [
+                  '''file_object = open(“filename”, “mode”) where\nfile_object is the variable to add the file object.''',
+                  '''file = open("testfile.txt","w")\nfile.write("Hello World")\nfile.close()''',
+              ],
+          },
+      ],
+  }
 
   # insert url here
-  uri = URI.parse("http://localhost:4567/response")
+  uri = URI.parse("http://0.0.0.0:6060/source-codes")
   # insert url here
 
   header = {"Content-Type" => 'application/json'}

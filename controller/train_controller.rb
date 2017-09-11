f684426f2bcf670a313c65637aa1bacd1e0d7e73
ahmadk3 <ahmad.aktb@gmail.com>
2017-09-10 21:48:18 -0300

    hotfix

--- controller/train_controller.rb
+++ controller/train_controller.rb
@@ -3,26 +3,35 @@ require 'date'
 require 'json'
 require 'nokogiri'
 
-puts "###############################################"
-startNode = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-train = Train.new( startNode, 5)
-train.getTrainData
 
+puts "###############################################"
+ini = DateTime.now.strftime('%Q').to_i
+
+if ARGV.count != 0
+  limit = ARGV[0].to_i
+else
+  limit = 50
+end
+
+# instantiate train object
+startNode = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+train = Train.new( startNode, limit)
+train.getTrainData
 data = train.json
 
 # insert url here
 uri = URI.parse("http://0.0.0.0:6060/train-network")
-# insert url here
 
+# make post
 header = {"Content-Type" => 'application/json'}
 http = Net::HTTP.new(uri.host, uri.port)
 request = Net::HTTP::Post.new(uri.request_uri, header)
 request.body = (data)
-#request.body.force_encoding("UTF-8")
 request.body
-# Send the request
 response = http.request(request)
 
 puts "POST made: "
 puts "\t doc count: " + train.docs.count.to_s
+fim = DateTime.now.strftime('%Q').to_i
+puts "\t Total time: " + (fim - ini).to_s
 puts "###############################################"

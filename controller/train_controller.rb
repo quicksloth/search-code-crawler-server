afb5be2f56c14234544950f1245440b605614f58
ahmadk3 <ahmad.aktb@gmail.com>
2017-10-08 01:06:11 -0300

    Network training data now sent via pushs

--- controller/train_controller.rb
+++ controller/train_controller.rb
@@ -7,28 +7,21 @@ require 'nokogiri'
 puts "###############################################"
 ini = DateTime.now.strftime('%Q').to_i
 
-if ARGV.count != 0
+if ARGV.count == 2
   limit = ARGV[0].to_i
+  pushLimit = ARGV[1].to_i
 else
   limit = 50
+  pushLimit = 50
 end
 
 # instantiate train object
 startNode = "https://en.wikipedia.org/wiki/Python_(programming_language)"
-train = Train.new( startNode, limit)
+train = Train.new( startNode, limit, pushLimit)
 train.getTrainData
-data = train.json
-
-# insert url here
-uri = URI.parse("http://0.0.0.0:10443/train-network")
 
 # make post
-header = {"Content-Type" => 'application/json'}
-http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.request_uri, header)
-request.body = (data)
-request.body
-response = http.request(request)
+
 
 puts "POST made: "
 puts "\t doc count: " + train.docs.count.to_s
--- model/train.rb
+++ model/train.rb
@@ -13,13 +13,16 @@ class Train
 
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-  def initialize rootUrl, urlsLimit
+  def initialize rootUrl, urlsLimit, pushlimit
     @rootUrl = rootUrl
     @docs = []
     @nodes = []
     @urlsLimit = urlsLimit
     @urlsCount = 0
     @accessedUrls = []
+    @pushlimit = pushlimit
+    @accessedUrlsFileName = "accessedurls.txt"
+    readAccessedUrls
   end
 
   def getTrainData url = @rootUrl
@@ -44,14 +47,25 @@ class Train
 
     # recursive call
     nextUrl = @nodes.shift
-    while @accessedUrls.include?(nextUrl)
+    while @accessedUrls.include?("https://en.wikipedia.org/" + nextUrl)
       nextUrl = @nodes.shift
+    end
+
+    if @urlsCount%@pushlimit == 0
+      puts "coc"
+      generateJson
+      #postData
+      writeAccessedUrls
+      readAccessedUrls
     end
 
     if @urlsCount < @urlsLimit && urls.size != 0
       getTrainData("https://en.wikipedia.org/" + nextUrl)
     else
       generateJson
+      postData
+      writeAccessedUrls
+      readAccessedUrls
     end
   end
 
@@ -63,7 +77,33 @@ class Train
     @json = h.to_json
   end
 
+  def postData
+    uri = URI.parse("http://0.0.0.0:10443/train-network")
+    data = @json
+    header = {"Content-Type" => 'application/json'}
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = (data)
+    http.request(request)
+    puts "Post successfully made to destination with #{@pushlimit} links."
+  end
+
+  def readAccessedUrls
+    urlsText = File.read(@accessedUrlsFileName)
+    @accessedUrls = urlsText.split("\n")
+  end
+
+  def writeAccessedUrls
+    File.open(@accessedUrlsFileName, 'w') { |f|
+      @accessedUrls.each do |url|
+        f.puts url
+      end
+    }
+  end
+
 end
+
+
 
 # startNode = "https://en.wikipedia.org/wiki/Python_(programming_language)"
 # train = Train.new( startNode, 10)

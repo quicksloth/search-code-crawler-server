3a1f22eeb4566a4f70014fb2411917d9c616adfb
pamepeixinho <pameiupip@gmail.com>
2017-11-15 21:18:05 -0200

    update local url

--- controller/crawler_controller.rb
+++ controller/crawler_controller.rb
@@ -48,7 +48,7 @@ after '/crawl' do
   data = everything(json["query"], json["requestID"], json["language"])
 
   # insert url here
-  uri = URI.parse("http://quickslothrecommendationserver.herokuapp.com/source-codes")
+  uri = URI.parse("http://0.0.0.0:10443/source-codes")
   # insert url here
 
   header = {"Content-Type" => 'application/json'}
